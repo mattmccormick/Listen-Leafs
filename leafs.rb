d61d97827e424de28270929d95070367b98d5490
Matt McCormick <matt@mattmccormick.ca>
2011-12-26 14:37:31 +0900

    Calculating time difference and launching separate process for listening

--- leafs.rb
+++ leafs.rb
@@ -1,11 +1,27 @@
 require 'open-uri'
 require 'nokogiri'
-#l = open('http://stats.cbc.ca/nhl/teamstats.asp?teamno=21&type=schedule')
+require 'tzinfo'
 
-doc = Nokogiri::HTML(open('espn.html'))
+doc = Nokogiri::HTML(open('http://espn.go.com/nhl/team/schedule/_/name/tor/toronto-maple-leafs'))
 doc.css('table.tablehead tr').each do |tr|
-	puts tr['class']
+	css_class = tr.attribute('class').to_str()
+	next if css_class == 'stathead' || css_class == 'colhead'
+	td = tr.children()
+
+	time_result = td[2].to_str()
+
+	if time_result.index('PM')	# find the first game that hasn't completed yet
+		tz = TZInfo::Timezone.get('America/New_York')
+		eastern_now = tz.now
+		timezone = eastern_now.dst?() ? 'EDT' : 'EST'
+		
+		day = td[0].to_str().slice(5..-1)	# just get the date without the day of the week
+		game_time = DateTime.parse(day + ' at ' + time_result + ' ' + timezone)
+		diff = game_time.to_time - Time.now
+
+		if diff < 60 * 10	# if within 10 minutes of game time
+			system('vlc http://5543.live.streamtheworld.com/CFMJAMAAC &')	
+		end
+		exit
+	end
 end
-# http://nokogiri.org/Nokogiri/XML/Node.html
-#- find next game (first blank result)
-#- if within time range, open the sports radio site
