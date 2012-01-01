419006e2c809faf5557e2af7eac41eeb04bbdeab
Matt McCormick <matt@mattmccormick.ca>
2012-01-01 11:53:38 +0900

    Updated deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,50 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+# replace these with your server's information
+set :domain,  "69.195.198.179:22123"
+set :user,    "matt"
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+# name this the same thing as the directory on your server
+set :application, "leafs"
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+# use your local repository as the source
+set :repository, "file://#{File.expand_path('.')}"
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+# or use a hosted repository
+#set :repository, "ssh://user@example.com/~/git/test.git"
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+server "#{domain}", :app, :web, :db, :primary => true
+
+set :deploy_via, :copy
+set :copy_exclude, [".git", ".DS_Store"]
+set :scm, :git
+set :branch, "master"
+# set this path to be correct on yoru server
+set :deploy_to, "/home/#{user}/www/#{application}"
+set :use_sudo, false
+set :keep_releases, 2
+set :git_shallow_clone, 1
+
+ssh_options[:paranoid] = false
+
+# this tells capistrano what to do when you deploy
+namespace :deploy do
+
+  desc <<-DESC
+  A macro-task that updates the code and fixes the symlink.
+  DESC
+  task :default do
+    transaction do
+      update_code
+      symlink
+    end
+  end
+
+  task :update_code, :except => { :no_release => true } do
+    on_rollback { run "rm -rf #{release_path}; true" }
+    strategy.deploy!
+  end
+
+  task :after_deploy do
+    cleanup
+  end
+
+end
