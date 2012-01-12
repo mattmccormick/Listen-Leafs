7c5f3b04879bb1ef2b96456d56a21c63368bad3a
Matt McCormick <matt@mattmccormick.ca>
2012-01-12 18:58:39 +0900

    Updated to make sure date is correct for file

--- streamripper.sh
+++ streamripper.sh
@@ -1,4 +1,4 @@
-DATE=`date -d +%Y-%m-%d`
+DATE=`date -d '-6 hours' +%Y-%m-%d` # because server is on UTC time
 DIR_FILES='/home/matt/leafs/'
 streamripper http://6703.live.streamtheworld.com/CFMJAMAAC -d $DIR_FILES -l 10800 -isAT -k 0 -a $DATE.aac
 ffmpeg -v 5 -y -i "${DIR_FILES}/${DATE}.aac.mp3" -acodec libmp3lame -ac 2 -ab 160k -aq 2 -metadata title="Toronto $1 - ${DATE}" -metadata author="AM 640 Toronto" "${DIR_FILES}/${DATE}.mp3"; 
