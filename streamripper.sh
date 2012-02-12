b2b9e95d10883c9d996e44a57f1dbae10513f809
Matt McCormick <matt@mattmccormick.ca>
2012-02-12 16:29:40 +0900

    Change to record 2:55

--- streamripper.sh
+++ streamripper.sh
@@ -1,7 +1,7 @@
 DATE=`date -d '-6 hours' +%Y-%m-%d` # because server is on UTC time
 DIR_FILES='/home/matt/leafs/'
 MP3_FILE=${DIR_FILES}${DATE}.mp3
-streamripper http://6703.live.streamtheworld.com/CFMJAMAAC -d $DIR_FILES -l 10800 -isAT -k 0 -a $DATE.aac
+streamripper http://6703.live.streamtheworld.com/CFMJAMAAC -d $DIR_FILES -l 10500 -isAT -k 0 -a $DATE.aac
 ffmpeg -v 5 -y -i "${DIR_FILES}/${DATE}.aac.mp3" -acodec libmp3lame -ac 2 -ab 96k "${MP3_FILE}"
 id3v2 -t "Toronto $1 - ${DATE}" -a "AM 640 Toronto" "${MP3_FILE}"
 ln -s $MP3_FILE /home/matt/www/mattmccormick.ca/public/leafs/ 
